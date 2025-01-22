284c35a18a5963b99e0eab206a0e57c83b2b17f3
Andi Fuerst <andifuerst13@gmail.com>
2025-01-21 21:50:59 -0500

    ITS-1 Used a CornerWidget to Move a Log In Menu option to the right corner

--- MainWindow/main_window_widget.py
+++ MainWindow/main_window_widget.py
@@ -1,7 +1,7 @@
 """
 This file handles the loading and configuring of the Main Window.
 """
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QMenuBar, QMenu
 from PyQt5.uic import loadUi
 
 
@@ -13,3 +13,8 @@ class MainWindow(QMainWindow):
         super().__init__()
         loadUi("MainWindow/main_window.ui", self)
         
+        # Adding a Log In Menu Option on Right Side of Screen
+        new_menu_bar = QMenuBar(self.menu_bar)
+        menu = QMenu("Log In", new_menu_bar)
+        new_menu_bar.addMenu(menu)
+        self.menu_bar.setCornerWidget(new_menu_bar)
